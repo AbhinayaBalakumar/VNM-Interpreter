9a319999d74c0bdee3b0f065af92a69d3e16f958
Abhi <161173993+AbhinayaBalakumar@users.noreply.github.com>
2026-04-29 21:59:52 -0400

    Add IdBoolToken class for token representation

---
+++ Part1/IdBoolToken.java
@@ -0,0 +1,20 @@
+public class IdBoolToken extends Token {
+    public String value;
+    
+    public IdBoolToken(int kind, String image) {
+        this.kind = kind;
+        this.image = image;
+        this.value = image;
+    }
+    
+    @Override
+    public Object getValue() {
+        return value;
+    }
+    
+    @Override
+    public String toString() {
+        return value;
+    }
+}
+
